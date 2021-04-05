6dfc1b15ba46e72767eb9871d43a4adbafc870db
Eaman Elbatiti <eelbatiti@catalystsolves.com>
2021-04-05 17:19:37 -0500

    Initial Commit for Pushing to Git Hub

---
+++ src/app/App.js
@@ -0,0 +1,42 @@
+import style from './App.module.css';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { hitCountUrl, getCountUrl } from '../utils/Url';
+
+/***
+ * This is a proof of concept for a counting application.
+ */
+function App() {
+
+  const [ count, setCount ] = useState();
+
+  // on load, fetch count from the url.
+  useEffect(() => {
+    const fetchCount = async () => {
+      const result = await axios(
+        `${getCountUrl}`
+      );
+      setCount(result.data.value);
+    };
+    fetchCount();
+    }, []);
+
+  // When the hit button is clicked, fetch and increment count from the url.
+  const incrementCount = async () => {
+    const result = await axios(
+      `${hitCountUrl}`
+    );
+    setCount(result.data.value);
+  };
+
+  return (
+    <div className= {style.body}>
+      <p>Number Of Hits</p>
+     <div className= {style.count}>{count}</div>
+     <button onClick={incrementCount} className={style.button}>Hit</button>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/utils/Url.js
@@ -0,0 +1,2 @@
+export const hitCountUrl = 'https://api.countapi.xyz/hit/1ccb732e-b55a-4404-ad3f-0f99c02fe44e';
+export const getCountUrl = 'https://api.countapi.xyz/get/1ccb732e-b55a-4404-ad3f-0f99c02fe44e';
